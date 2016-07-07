5269578d8eec4e3674617578596da6ea5b93a94b
Rajan Middha <rajan@Rajans-MacBook-Air.local>
2016-07-07 15:31:51 +0530

    catch and count all exceptions

--- python/exception_logs.py
+++ python/exception_logs.py
@@ -4,11 +4,12 @@ import json
 import requests
 import time
 
-android_count=ios_count=web_count=unknown_count = 0
+reason_count=android_count=ios_count=web_count=unknown_count = 0
+exception_reason = {}
 
 for file in os.listdir('/Users/rajan/logs'):
 	print 'Looking in file: '+file
-	time.sleep(2)
+	time.sleep(1)
 	with open('/Users/rajan/logs/'+file,'r') as infile:
 		for line in infile:
 			data = json.loads(line)
@@ -18,6 +19,10 @@ for file in os.listdir('/Users/rajan/logs'):
 			device_req = requests.get('http://api.pressplaytv.in/fetch/device/'+device_id)
 			device_response = device_req.json()
 			
+			if reason not in exception_reason:
+				exception_reason[reason] = 0
+			exception_reason[reason] += 1				
+
 			if 'status' in device_response:
 				if (device_response['status'] == 'failed'):
 					unknown_count += 1
@@ -44,10 +49,10 @@ for file in os.listdir('/Users/rajan/logs'):
 				elif source == 'web':
 					if os_family == 'iOS':
 						ios_count += 1 
-						web_count =+ 1
+						web_count += 1
 					elif os_family == 'Android':
 						android_count += 1
-						web_count =+ 1
+						web_count += 1
 					else:
 						unknown_count += 1
 						web_count += 1
@@ -55,10 +60,12 @@ for file in os.listdir('/Users/rajan/logs'):
 				unknown_count += 1			
 				
 #			print 'user_id: '+user_id,'device_id: '+device_id,'source: '+source,'os_family: '+os_family
-
 #			time.sleep(1)
 #			print 'ANDROID: '+str(android_count)+'   '+'IOS: '+str(ios_count)+'   '+'WEB: '+str(web_count)+'   '+'UNKNOWN: '+str(unknown_count)
-print 'Total ANDROID: '+str(android_count)
-print 'Total IOS: '+str(ios_count)
-print 'Total WEB: '+str(web_count)
-print 'Total UNKNOWN: '+str(unknown_count)
+print 'Total ANDROID: ',android_count
+print 'Total IOS: ',ios_count
+print 'Total WEB: ',web_count
+print 'Total UNKNOWN: ',unknown_count
+print 'Exception Reason: '
+for key,value in exception_reason.iteritems():
+	print 'Reason: ',key,'Count: ',value
