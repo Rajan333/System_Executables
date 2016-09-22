0fd0a1d8a4882c2338614429bb93394a69d90b3d
Rajan Middha <middha.rajan@gmail.com>
2016-09-22 16:16:39 +0530

    changes in server.rb

--- chef-recepies/server.rb
+++ chef-recepies/server.rb
@@ -8,7 +8,7 @@ end
 #Install oh-my-zsh and git
 
 execute 'installing-zsh-and-git' do
-	command 'apt-get install zsh git-core -y && wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh && chsh -s `which zsh`'
+	command 'apt-get install zsh git-core -y && wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh && chsh -s `which zsh` ubuntu'
 action :run
 end
 
